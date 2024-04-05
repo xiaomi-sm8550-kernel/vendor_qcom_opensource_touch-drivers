72063c31fb72e4bb9a29c66b6413c5a9f1cc0ae7
Syed Ahmed <quic_syedahme@quicinc.com>
2024-04-05 17:06:09 +0530

    touch: bengal: enable focaltech touch driver
    Enable focaltech touch driver for bengal target.
    
    Change-Id: I5bf0cb67050663a3efbf1885ac4d36cfffc391d3
    Signed-off-by: Syed Ahmed <quic_syedahme@quicinc.com>

--- touch_driver_board.mk
+++ touch_driver_board.mk
@@ -33,7 +33,8 @@ ifeq ($(TOUCH_DLKM_ENABLE),  true)
                                 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/goodix_ts.ko
                         else ifeq ($(TARGET_BOARD_PLATFORM), bengal)
                                 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/synaptics_tcm_ts.ko \
-                                        $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko
+                                        $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko \
+					$(KERNEL_MODULES_OUT)/focaltech_fts.ko
                         else ifeq ($(TARGET_BOARD_PLATFORM), trinket)
                                 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/synaptics_tcm_ts.ko
                         else ifeq ($(TARGET_BOARD_PLATFORM), pitti)
